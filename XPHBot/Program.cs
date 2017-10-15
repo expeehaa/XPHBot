f7384493c254c30a75ba7bb3831da589a168f228
expeehaa <expeehaa@outlook.com>
2017-10-15 14:28:02 +0200

    Projektdateien hinzufügen.

---
+++ XPHBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XPHBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
